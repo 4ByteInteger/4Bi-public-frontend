a4f7c217f9d449ce7d3de8cad777efaefc01e1f3
SAMUDRALAARAVIND <aravindsamudrala1708@gmail.com>
2023-12-11 13:34:25 +0530

    added aws cloudfront endpoint as baseurl

--- src/network/Endpoints.ts
+++ src/network/Endpoints.ts
@@ -3,7 +3,10 @@
 // DEV 
 // const baseUrl = "https://fourbi-service.onrender.com"
 // Aws
-const baseUrl = "http://ec2-3-110-105-130.ap-south-1.compute.amazonaws.com:8080";
+// const baseUrl = "http://ec2-3-110-105-130.ap-south-1.compute.amazonaws.com:8080";
+
+// AWS cloudfront url
+const baseUrl = "https://dzm3ijcwfledm.cloudfront.net"
 
 export const Endpoints = {
     coursesList: `${baseUrl}/course/all`,
